49e3f472ee5c141609bc1dbe96d1b62b03e333a6
CarlCabral07 <carlcabral.xentra@gmail.com>
2024-02-26 21:45:59 +0800

    Code Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'xbank-cabral-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
